b85fa3aa04b6951adbadce47b89aa6a5db536bb7
root <vivek12194@gmail.com>
2017-05-04 15:54:59 -0400

    another one

--- envname/app.py
+++ envname/app.py
@@ -91,9 +91,11 @@ def makeWebhookResult(data):
     
     # if result is None:
     #     return {}
+
  	for x in data:
- 		speech = speech + "Here is a list  "+ str(x['name']) + str(x['location']) + "\n"
-	
+ 		speech = speech + str(x['name']) + str(x['location']) + "\n"
+
+	speech= "Here is the list " + speech
 	return {
     "speech": speech,
     "displayText": speech,
