dac984b758edc1aa920e1a4d5c81b4da016b6bee
root <vivek12194@gmail.com>
2017-05-06 14:49:29 -0400

    another one

--- envname/app.py
+++ envname/app.py
@@ -98,7 +98,7 @@ def makeWebhookResult1(data):
     n=""
     new =""
     for x in data:
-        n = str(x['name']) + str(x['rating_img_url_small']) + "\n"
+        n = str(x['name']) + str(x['rating']) + "\n"
         new = new + n
 
 
