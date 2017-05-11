bbe0f1843ff72b4376de17f390fffaa07287b0f8
root <vivek12194@gmail.com>
2017-05-11 00:35:38 -0400

    another one

--- envname/app.py
+++ envname/app.py
@@ -84,7 +84,7 @@ def webhook():
 
     # print (req)
     es = Elasticsearch(hosts=[{'host': host,'port':port}],use_ssl=True,verify_certs=True,connection_class=RequestsHttpConnection)
-    res = es.search(size=5,index="fb", body={"query": {"match":{"type":req}}})
+    res = es.search(size=5,index="fb", body={"query": {"match":{"type":req, "location":place}}})
     #res = es.get(index="fb")
     listOfDicts = []
     listOfRating=[]
@@ -104,22 +104,6 @@ def webhook():
     else :
         res=makeWebhookResult(listOfImage)
 
-
-
-def processRequest1(req):
-
-    
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult3(data)
-    return res
-
-
     # res=json.dumps(res,indent=4)
     # r = make_response(res)
     # r.headers['Content-Type'] = 'application/json'
@@ -130,80 +114,6 @@ def processRequest1(req):
     res=make_response(res)
     res.headers['Content-Type'] = 'application/json'
     return res
-
-
-
-def makeYqlQuery(req):
-    city = "New York"
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='New York')"
-
-
-def makeWebhookResult3(data):
-    query = data.get('query')
-    if query is None:
-
-        return {}
-    result = query.get('results')
-    if result is None:
-
-        return {}
-    channel = result.get('channel')
-    if channel is None:
-
-        return {}
-
-
-
-    item = channel.get('item')
-
-    location = channel.get('location')
-
-    units = channel.get('units')
-
-    if (location is None) or (item is None) or (units is None):
-
-        return {}
-
-
-
-    condition = item.get('condition')
-
-    if condition is None:
-
-        return {}
-
-
-
-    # print(json.dumps(item, indent=4))
-
-
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-
-
-    print("Response:")
-
-    print(speech)
-
-
-
-    return {
-
-        "speech": speech,
-
-        "displayText": speech,
-
-        # "data": data,
-
-        # "contextOut": [],
-
-        "source": "apiai-weather-webhook-sample"
-
-    }
-
-
-    
 
 
 def processRequest(req):
