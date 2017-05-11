3e073f52d44a002afa8eb72eceeecd7f28acbc2d
root <vivek12194@gmail.com>
2017-05-11 16:33:17 -0400

    ah

--- envname/app.py
+++ envname/app.py
@@ -92,11 +92,11 @@ def webhook():
             listOfRating.append(sourceValue['rating'])
             listOfImage.append(sourceValue)
             listOfDicts.append(''.join([i if ord(i) < 128 else '' for i in text])) 
-            res =makeWebhookResult1(listOfImage)   # print (listOfDicts)
-            res=json.dumps(res,indent=4)
-            res=make_response(res)
-            res.headers['Content-Type'] = 'application/json'
-            return res
+         res =makeWebhookResult1(listOfImage)   # print (listOfDicts)
+        res=json.dumps(res,indent=4)
+        res=make_response(res)
+        res.headers['Content-Type'] = 'application/json'
+        return res
 
     elif ac=="previousContext":
         #ac=my_previous_action
@@ -260,42 +260,42 @@ def makeWebhookResult2(data1):
         'source':'Yelp'}
 
 def makeWebhookResult1(data):
-    
-    dict_of_elements=[]
-    for i in data:
-        ducs={}
-        ducs['title']=i['name']
-        ducs['image_url']=i['image_url']
-        ducs['subtitle']=i['price']
-        new={}
-        new['type']='web_url'
-        new['url']=i['url']
-        new['title']='View website'
-        newPhone={}
-        newPhone['type']='phone_number'
-        newPhone['payload']=i['phone']
-        newPhone['title']='Call'
-        newList=[]
-        #mapOpener['payload']=location+''.join(str(x) for x in i['location']).replace(' ','+')
-        newList.append(new)
-        newList.append(newPhone)
-        #newList.append(mapOpener)
-        ducs['buttons']=newList
-        dict_of_elements.append(ducs)
-    return {'speech':"Here is a list",
-        "displayText":"Here is a list",
-        "data":{
-        'facebook':{
-        "attachment": {
-        "type": "template",
-        "payload": {
-        "template_type": "generic",
-        "elements": dict_of_elements
-        }
-        }
-        }},
-    source:'API'}
-    
+        
+        dict_of_elements=[]
+        for i in data:
+            ducs={}
+            ducs['title']=i['name']
+            ducs['image_url']=i['image_url']
+            ducs['subtitle']=i['price']
+            new={}
+            new['type']='web_url'
+            new['url']=i['url']
+            new['title']='View website'
+            newPhone={}
+            newPhone['type']='phone_number'
+            newPhone['payload']=i['phone']
+            newPhone['title']='Call'
+            newList=[]
+            #mapOpener['payload']=location+''.join(str(x) for x in i['location']).replace(' ','+')
+            newList.append(new)
+            newList.append(newPhone)
+            #newList.append(mapOpener)
+            ducs['buttons']=newList
+            dict_of_elements.append(ducs)
+        return {'speech':"Here is a list",
+            "displayText":"Here is a list",
+            "data":{
+            'facebook':{
+            "attachment": {
+            "type": "template",
+            "payload": {
+            "template_type": "generic",
+            "elements": dict_of_elements
+            }
+            }
+            }},
+        source:'API'}
+        
 
 
 def makeWebhookResult(data):
